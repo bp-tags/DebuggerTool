10f8efef7dc3d1c8e1b0305d99dfd8011f2e6424
dkss <dkss0309@qq.com>
2017-11-15 02:43:49 +0800

    整合
    第一版没有修改太多原始代买，报错很多的版本

---
+++ Assets/Script/Debugger/DebugerSetting.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugerSetting {
+
+    public bool LockScroll = false;
+    public bool InfoFilter = false;
+    public bool WarningFilter = false;
+    public bool ErrorFilter = false;
+    public bool FatalFilter = false;
+}
